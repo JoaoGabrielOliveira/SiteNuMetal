c1ea20c75482cc4428994035f00275d744c98fed
JoaoGabrielOliveira <50085959+JoaoGabrielOliveira@users.noreply.github.com>
2019-12-12 13:38:48 -0300

    Atualização do Front-end
    Ainda não funcional

--- conteudo/codigos/main.js
+++ conteudo/codigos/main.js
@@ -1,3 +1,19 @@
 function Apagar(){
-    var 
+    
+}
+
+function mudarFundo(CaminhoImage){
+    console.log("Mudou?");
+    document.getElementById('corpo').style.backgroundImage = 'url(' + CaminhoImage + ')';
+}
+
+function PegarDados(){
+    var url = 'https://api.spotify.com/v1/artists/3RNrq3jvMZxD9ZyoOZbQOD';
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", url, false);
+
+    xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
+
+    return xhttp.responseText;
 }
