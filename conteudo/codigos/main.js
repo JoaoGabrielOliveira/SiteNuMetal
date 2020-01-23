83cedf191942e84460b37337d947cdb1058d2278
JoaoGabrielOliveira <50085959+JoaoGabrielOliveira@users.noreply.github.com>
2020-01-23 12:48:40 -0300

    ver 0.2
    # Menu melhorado, agora também responsivel.

--- conteudo/codigos/main.js
+++ conteudo/codigos/main.js
@@ -1,6 +1,3 @@
-function Apagar(){
-    
-}
 
 function mudarFundo(CaminhoImage){
     console.log("Mudou?");
@@ -16,4 +13,22 @@ function PegarDados(){
     xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
 
     return xhttp.responseText;
+}
+
+function AbirirMenu(){
+    if (document.getElementsByClassName("MenuItem")[0].style.display == "block") {
+
+        document.getElementsByClassName("MenuItem")[0].style.display = "none";
+        document.getElementsByClassName("MenuItem")[1].style.display = "none";
+        document.getElementsByClassName("MenuItem")[2].style.display = "none";
+        document.getElementById("Menu").style.background = "transparent";
+    }
+
+    else {
+        document.getElementsByClassName("MenuItem")[0].style.display = "block";
+        document.getElementsByClassName("MenuItem")[1].style.display = "block";
+        document.getElementsByClassName("MenuItem")[2].style.display = "block";
+        document.getElementById("Menu").style.background = "aliceblue";
+    }
+    
 }
